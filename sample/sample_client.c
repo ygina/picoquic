533fd159ebe65a7e92cdfd9380aad27cd455e2a4
Gina Yuan <gina.yuan@gmail.com>
2025-04-23 20:23:36 +0000

    Hardcode the reset frequency in each application client

--- sample/sample_client.c
+++ sample/sample_client.c
@@ -532,7 +532,8 @@ int picoquic_sample_client(char const * server_name, char const * cca,
         &server_address, &quic, &cnx, &client_ctx);
 
     if (quacker) {
-        quic->quacker = udp_quacker_new(threshold, freq_pkts, freq_ms, target_addr, riblt, optimistic_cache_policy);
+        uint64_t reset_freq_ms = 10; // TODO: hardcoded
+        quic->quacker = udp_quacker_new(threshold, freq_pkts, freq_ms, target_addr, riblt, optimistic_cache_policy, reset_freq_ms);
         quic->quacker_riblt = riblt;
         quic->quacker_hint = hint;
     } else {
